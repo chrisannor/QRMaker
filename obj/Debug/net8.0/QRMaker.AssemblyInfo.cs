8a0304dfc116cfb05ca76e6a46ba84dd26442f5a
Chris Annor <annorc@kynetix.com>
2025-07-29 10:40:26 +0100

    Merge remote changes

--- obj/Debug/net8.0/QRMaker.AssemblyInfo.cs
+++ obj/Debug/net8.0/QRMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QRMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c515855a4cd8aa16341192ad23be03552bb98815")]
 [assembly: System.Reflection.AssemblyProductAttribute("QRMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QRMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
